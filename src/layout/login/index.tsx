a94878cd217314cd3782ee183121726b4d8934a9
lfb0728 <1449826851@qq.com>
2021-05-25 23:46:22 +0800

    feat(项目搭建): 集成国际化

--- src/layout/login/index.tsx
+++ src/layout/login/index.tsx
@@ -1,8 +1,10 @@
 import { defineComponent } from '@vue/runtime-core';
 import styles from './index.module.less';
+import { useI18n } from 'vue-i18n';
 console.log(styles);
 export default defineComponent({
   setup() {
+    const { t } = useI18n();
     return () => (
       <div class={styles['login-wrapper']}>
         <div class={styles['header-wrapper']}></div>
@@ -11,6 +13,7 @@ export default defineComponent({
             <a-form-item>
               <a-input />
             </a-form-item>
+            <a-button>{t('login')}</a-button>
           </a-form>
         </div>
       </div>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import App from './App';
+import { createI18n } from 'vue-i18n';
 import router from './router';
 import store from './store';
 import 'normalize.css';
@@ -8,4 +9,19 @@ import '@/styles/index.less';
 import './permission';
 const app = createApp(App);
 registerAntdUIs(app);
-app.use(router).use(store).mount('#app');
+
+const i18n = createI18n({
+  legacy: false,
+  locale: localStorage.getItem('locale') || 'zh',
+  fallbackLocale: 'zh',
+  messages: {
+    en: {
+      login: 'login',
+    },
+    zh: {
+      login: '登录',
+    },
+  },
+});
+
+app.use(i18n).use(router).use(store).mount('#app');
